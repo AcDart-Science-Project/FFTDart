cee293f091d4f8f01ac5fffeca9cf7780548b51b
Furcht968 <tomu.iwanaga129+github@gmail.com>
2021-01-23 22:47:38 +0900

    add: dart simple package追加、description, README.md編集

---
+++ lib/FFTDart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library FFTDart;
+
+export 'src/FFTDart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
